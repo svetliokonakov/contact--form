f0e302ddd0ed4b97c313fd167ced0d872ea6a3a6
svetliokonakov <66563165+svetliokonakov@users.noreply.github.com>
2020-06-10 15:49:01 +0300

    Add files via upload

--- contact form/form-submit.php
+++ contact form/form-submit.php
@@ -6,5 +6,13 @@
 <body>
 	Welcome <?php echo $_GET['fname'] . $_GET['lname']; ?><br>
 	Your email adress is: <?php echo $_GET["email"]; ?>
+
+	<?php			
+		$csvFile = "myCSV.csv";
+		$fh = fopen($csvFile, 'a',) or die("can't open file");
+		$stringData = $_GET['fname'] .','. $_GET['lname']. ',' .$_GET['email'];
+		fwrite($fh, $stringData);
+		fclose($csvFile);
+	?>
 </body>
 </html>
