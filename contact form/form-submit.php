f3f83913ea37545908319eaffc9d1db47e5fd112
svetliokonakov <66563165+svetliokonakov@users.noreply.github.com>
2020-06-10 14:30:20 +0300

    Add files via upload

---
+++ contact form/form-submit.php
@@ -0,0 +1,10 @@
+<?php error_reporting(-1);?>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	Welcome <?php echo $_GET['fname'] . $_GET['lname']; ?><br>
+	Your email adress is: <?php echo $_GET["email"]; ?>
+</body>
+</html>
---
+++ contact form/index.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+  <title>Contact Form</title>
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</head>
+<body>
+
+	<form action="form-submit.php" method="get">		
+		<div class="names">	
+  			<label for="fname">First name:</label>
+
+  			<input type="text" id="fname" name="fname">
+
+    		<label for="lname">Last name:</label>
+
+  			<input type="text" id="lname" name="lname"><br>
+  		</div>
+
+		<div class="email>">
+			<label for="email">Email:</label>
+
+			<input type="text" id="email" name="email"><br>
+
+  			<input type="submit" value="Submit">
+		</div>
+
+    	<div class="message">
+    		<label for="message"></label>
+
+  			<textarea id="message" name="message" rows=4 cols=50 placeholder="Write something here..."></textarea> 	  
+   	 	</div>
+	</form>		
+
+</body>
+</html>
